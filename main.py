2990b152a92376574fa93ff206a0a28d46243935
MedrekIT <dan.creez@o2.pl>
2025-04-28 04:24:37 +0200

    Created player's physics

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        pygame.display.flip()
 
 if __name__ == '__main__':
-    main()
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers  = (updatable, drawable)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
